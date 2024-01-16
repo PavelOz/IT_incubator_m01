ed8564da8d36accdca16ea6642af7f7e6f414289
Pavel Ozernov <pavel.ozernov@gmail.com>
2024-01-16 18:07:02 +0600

    adds Main adaptive

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,12 +4,13 @@ import photo from "../../../assests/images/main.webp";
 import { FlexWrapper } from "../../../components/FlexWrapper";
 import { Container } from "../../../components/Container";
 import { theme } from "../../../styles/Theme";
+import { font } from "../../../styles/Common";
 
 export const Main = () => {
   return (
     <StyledMain>
       <Container>
-      <FlexWrapper align={"center"} justify={"space-between"}>
+      <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
         <div>
           <SmallText>Hi there</SmallText>
           <Name>I am <span>Svetlana Dyablo</span></Name>
@@ -33,11 +34,14 @@ display: flex;
 const PhotWrapper = styled.div `
   position: relative;
   z-index: 0;
+  margin-top: 65px;
 
   &::before{
     content: "";
     width: 360px;
     height: 470px;
+
+    
 
     border: 5px solid ${theme.colors.accent};
     position: absolute;
@@ -45,7 +49,17 @@ const PhotWrapper = styled.div `
     left: 22px;
 
     z-index: -1;
+
+    @media ${theme.media.mobile} {
+    width: 314px;
+    height: 414px;
+    top:-17px;
+    left: 20px;
+
   }
+  }
+
+
 
 `
 
@@ -53,18 +67,27 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  margin-right: 20px;
+
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `;
 
 const MainTitle = styled.h1 `
-font-size: 27px;
-font-weight: 400;
+${font({weight: 400, Fmax: 27, Fmin: 20,})}
+
+/* font-size: 27px;
+font-weight: 400; */
 
 `
 const Name = styled.h2 `
+${font({family: "'Josefin Sans', sans-serif", weight: 700, Fmax: 50, Fmin: 36,})}
 
-font-family: Josefin Sans, sans-serif;
+/* font-family: Josefin Sans, sans-serif;
 font-size: 50px;
-font-weight: 700;
+font-weight: 700; */
 letter-spacing: 2.5px;
 
 margin: 10px 0;
@@ -72,6 +95,7 @@ margin: 10px 0;
 span{
 position: relative;
 z-index: 0;
+white-space: nowrap;
 
   &::before{
     content: "";
@@ -85,8 +109,14 @@ z-index: 0;
   }
 }
 
+@media ${theme.media.mobile} {
+  margin: 15px 0 22px
+  
+}
 `
 const SmallText = styled.h2 `
-font-size: 14px;
-font-weight: 400;
+${font({weight: 400, Fmax: 14, Fmin: 14,})}
+
+/* font-size: 14px;
+font-weight: 400; */
 `
